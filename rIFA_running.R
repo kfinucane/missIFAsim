13029cb4b4e6a96f51834c41e3441ce4c863f258
kfinucane <kate.finucane@ucdconnect.ie>
2026-04-28 17:22:18 +0100

    directory updated

--- rIFA_running.R
+++ rIFA_running.R
@@ -1,3 +1,4 @@
+# source model script
 source("rIFA_model.R")
 
 p <- 1391  # number of variables
@@ -11,7 +12,7 @@ sim_num <- 1  # which dataset to run
 chain <- 1  # which chain to run
 
 # load simulated data
-load(paste0(directory, "/../", sim_data_dist, "_dist_dataset_p_", p, "_", sim_num, ".RData"))
+load(paste0(directory, "/", sim_data_dist, "_dist_dataset_p_", p, "_", sim_num, ".RData"))
 
 # remove variables that have > 25% missingness
 # missingness by variable
@@ -38,8 +39,10 @@ if (length(which(prop_missing_by_var > 0.25)) > 0) {
   generated_data$use_data$mu <- generated_data$data_gen$mu
 }
 
+# seed
 set.seed(generated_data$seeding + sim_num)
 
+# run model
 rIFA_res <- col_rifa_gibbs(input_data = generated_data$use_data$data_w_missing,
                            coding = NA,
                            n.iters = 10000,
@@ -47,9 +50,10 @@ rIFA_res <- col_rifa_gibbs(input_data = generated_data$use_data$data_w_missing,
                            use_true_params = FALSE,
                            k.star = 5,
                            verbose = TRUE,
-                           burn = 0, thin = 5,
+                           burn = 5000, thin = 5,
                            return_chain = TRUE,
                            var_mult = 0.6,
-                           init_imp = "SVD")
+                           init_imp = "half_min")
 
+# save
 save(rIFA_res, file = paste0(directory, "/rIFA_MCMC_", sim_data_dist, "_p_", p, "_", sim_num, "_", chain, "_res.RData"))
