fa9ab7891f8abacbeeadb8d18b25734573300a1a
kfinucane <kate.finucane@ucdconnect.ie>
2026-04-28 17:14:50 +0100

    fixed directory location

--- TGIFA_running.R
+++ TGIFA_running.R
@@ -1,15 +1,17 @@
+# source necessary scripts
 source("tGIFA_model.R")
 source("tGIFA_utils.R")
 
+# load necessary packages
 library(softImpute)
 library(Rfast)
 
-dataset_num <- 1
-sim_num <- 1
+sim_num <- 1 # which simulated dataset to run
 
-p <- 1391
+p <- 1391  # number of variables
 sim_data_dist <- "gaussian"  # distribution desired
 
+# proposal distribution covariance multipliers
 chosen_prop_mult <- list("mu_prop_mult" = 0.05,
                          "eta_prop_mult" = 3L,
                          "lambda_prop_mult" = 3L,
@@ -20,7 +22,7 @@ chosen_prop_mult <- list("mu_prop_mult" = 0.05,
 directory <- paste0("results/p_", p, "_datasets")
 
 # load simulated data
-load(paste0(directory, "/../", sim_data_dist, "_dist_dataset_p_", p, "_", sim_num, ".RData"))
+load(paste0(directory, "/", sim_data_dist, "_dist_dataset_p_", p, "_", sim_num, ".RData"))
 
 # remove variables that have > 25% missingness
 # missingness by variable
@@ -47,16 +49,19 @@ if (length(which(prop_missing_by_var > 0.25)) > 0) {
   generated_data$use_data$mu <- generated_data$data_gen$mu
 }
 
-
+# seed
 set.seed(generated_data$seeding + sim_num)
 
-TGIFA_res <- TGIFA_model(input_data = generated_data$use_data$data_w_missing, n.iters = 10000, k.star = 5,
+# run TGIFA model
+TGIFA_res <- TGIFA_model(input_data = generated_data$use_data$data_w_missing, n.iters = 10, k.star = 5,
                          burn = 0, thin = 5, return_chain = TRUE, prop_mult = chosen_prop_mult,
                          use_true_params = FALSE, generated_data = generated_data,
                          var_mult = 0.6,
                          init_imp = "half_min")
 
+# file to save
 filename <- paste0(directory, "/TGIFA_MCMC_", sim_data_dist, "_p_", p, "_", sim_num, "_1_res.RData")
 
+# save
 save(TGIFA_res, file = filename)
 
